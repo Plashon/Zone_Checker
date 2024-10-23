460261be78901dc838366fccd4b127ee1c41a691
Plashon <fourza12poobate@gmail.com>
2024-10-23 19:35:30 +0700

    btn

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <div>
       <footer className="footer footer-center text-base-content rounded p-4 bg-base-300 fixed bottom-0 left-0 w-full">
         <nav className="grid grid-flow-col gap-2">
-          <a href="/aboutus" className="link link-hover text-sm">
+          <a href="/aboutus" className=" btn link link-hover text-sm">
             About us
           </a>
-          <a href="/contact" className="link link-hover text-sm">
+          <a href="/contact" className=" btn link link-hover text-sm">
             Contact
           </a>
         </nav>
